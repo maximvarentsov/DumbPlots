cd5673c9904b71b001189271b0f2aa0afe421c4e
turt2live <travpc@gmail.com>
2013-11-25 09:54:42 -0700

    Note to self

--- src/main/java/com/turt2live/dumbplots/DumbPlots.java
+++ src/main/java/com/turt2live/dumbplots/DumbPlots.java
@@ -21,6 +21,7 @@ import com.turt2live.dumbplots.terrain.TerrainGenerator;
 public class DumbPlots extends PluginWrapper implements Listener {
 
 	// TODO: Less creation of objects, more managers. Rewrite corners and plot data store
+	// TODO: Save and load plot corners. Doesn't load/save correctly
 
 	private static DumbPlots instance;
 	public static final int CHUNKS_PER_CORNER_FILE = 128;
